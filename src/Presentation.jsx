446113e368ac072801c21390b8a97474040afa88
Michael Mior <mmior@mail.rit.edu>
2023-02-13 11:37:48 -0500

    Add additional slides

--- src/Presentation.jsx
+++ src/Presentation.jsx
@@ -121,6 +121,143 @@ const Presentation = () => (
         - Contains important guidelines and policies
        `}
     </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # Regrading
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # Major topic in CS
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # Beckman Report
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # DB Education
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # Vs of Big Data
+
+        1. Volume - there is a lot of data
+        2. Variety - data items look different
+        3. Velocity - new data is arriving *fast*
+        4. Veracity - is the data correct?
+        5. Value - can we do anything with it?
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # Jobs
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # What is a datbase?
+
+        - Contains information about a particular enterprise
+        - Collection of interrelated data
+        - Set of programs to access that data
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # Examples
+
+        - Banking: accounts, transactions, etc
+        - Airlines: reservations, schedules
+        - Universities: registration (SIS), grades
+        - …
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # History
+
+        1. Files
+        2. File systems
+        3. Relational databases (SQL)
+        4. NoSQL
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # Files
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # File Systems
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # Problems with files
+
+        1. **Isolation**: Can many users use the DB?
+        2. **Integrity**: Is the data valid?
+        3. **Atomicity**: What if I fail while updating?
+
+        (see section 1.2 of the textbook)
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # Relational Databases
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # One Sizes Does Not Fit All
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # LinkedIn
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # Facebook
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # Google
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # Why not just learn NoSQL?
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # SQL is big business!
+       `}
+    </MarkdownSlide>
+    <MarkdownSlide>
+      {`
+        # History
+
+        - **1950s-1960s**: Large files on tapes
+        - **1960s-1970s**: Hard disks,
+                           Relational model
+        - **1980s**: R
+        - **1990s**: Decision support,
+                     Parallel DBMS,
+                     Growth of the WWW
+        - **2000s**: XML/XQuery,
+                     Open source systems,
+                     “NoSQL”
+       `}
+    </MarkdownSlide>
   </Deck>
 );
 
